8396841bdd889209641313b869fa972f1332d898
Ali <anix1@kent.edu>
2022-09-21 12:36:17 -0600

    add the classes and test suites back to how originally were

---
+++ src/classes/TravelerRepository.js
@@ -0,0 +1,10 @@
+class TravelerRepository {
+    constructor(travelData) {
+        this.travelers = travelData || [];
+    }
+    getTravelerInfoById(ID) {
+        return this.travelers.find(traveler => traveler.id === ID)
+    }
+}
+
+export default TravelerRepository;
